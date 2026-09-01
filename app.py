d58348435eff76ed1bc4f62bca01588b1952f496
Maiara Macedo <maiaramacedo.r@outlook.com>
2026-09-01 15:29:59 -0300

    feat: altera mensagem principal versão 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    return "Checkpoint funcionando :)!"
+    return "Checkpoint funcionando, versão 2 :)!"
 
 @app.route("/status")
 def status ():
